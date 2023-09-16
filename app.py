8675bbdc2b46fd11ad37a0f48517cde35c4a6dc9
kiyu99 <145136480+kiyu99@users.noreply.github.com>
2023-09-16 11:16:02 +0900

    app.py

--- app.py
+++ app.py
@@ -8,8 +8,9 @@ openai.api_key = st.secrets.OpenAIAPI.openai_api_key
 # st.session_stateを使いメッセージのやりとりを保存
 if "messages" not in st.session_state:
     st.session_state["messages"] = [
-        {"role": "system", "content": st.secrets.AppSettings.chatbot_setting}
-        ]
+        {"role": "system", "content": st.secrets.AppSettings.chatbot_setting},
+        {"role": "bot", "content": "今日は何を学びましたか？"}
+    ]
 
 # チャットボットとやりとりする関数
 def communicate():
